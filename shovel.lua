da0865c307e703e4870a2778916758d510376e5f
Erwin Graanstra <kurdt94@gmail.com>
2020-06-27 03:26:02 +0200

    Update shovel.lua

--- shovel.lua
+++ shovel.lua
@@ -13,33 +13,33 @@ function StartAnimation(animDict,flags,playbackListName,p3,p4,groundZ,time)
 		end
 
 		local animScene = Citizen.InvokeNative(0x1FCA98E33C1437B3, animDict, flags, playbackListName, 0, 1)
-			-- SET_ANIM_SCENE_ORIGIN
-	    Citizen.InvokeNative(0x020894BF17A02EF2, animScene, aCoord.x, aCoord.y, aCoord.z, pRot.x, pRot.y, pRot.z, 2) 
-	    -- SET_ANIM_SCENE_ENTITY
-	    Citizen.InvokeNative(0x8B720AD451CA2AB3, animScene, "player", player, 0)
+		-- SET_ANIM_SCENE_ORIGIN
+		Citizen.InvokeNative(0x020894BF17A02EF2, animScene, aCoord.x, aCoord.y, aCoord.z, pRot.x, pRot.y, pRot.z, 2) 
+		-- SET_ANIM_SCENE_ENTITY
+		Citizen.InvokeNative(0x8B720AD451CA2AB3, animScene, "player", player, 0)
 	    
-	    -- DIG UP A CHEST
-	    --local chest = CreateObjectNoOffset(GetHashKey('p_strongbox_muddy_01x'), pCoord, true, true, false, true)
-	    --Citizen.InvokeNative(0x8B720AD451CA2AB3, animScene, "CHEST", chest, 0)
+	    	-- DIG UP A CHEST
+	    	--local chest = CreateObjectNoOffset(GetHashKey('p_strongbox_muddy_01x'), pCoord, true, true, false, true)
+	    	--Citizen.InvokeNative(0x8B720AD451CA2AB3, animScene, "CHEST", chest, 0)
 
-	    -- LOAD_ANIM_SCENE
-	    Citizen.InvokeNative(0xAF068580194D9DC7, animScene) 
-	    Citizen.Wait(1000)
-	    -- START_ANIM_SCENE
-	    print('START_ANIM_SCENE: '.. animScene)
-	    Citizen.InvokeNative(0xF4D94AF761768700, animScene) 
-	    if time then
-	    	Citizen.Wait(tonumber(time))	
-	    else
-	   	Citizen.Wait(10000) 
-	    end
+	    	-- LOAD_ANIM_SCENE
+	    	Citizen.InvokeNative(0xAF068580194D9DC7, animScene) 
+	    	Citizen.Wait(1000)
+	    	-- START_ANIM_SCENE
+	    	print('START_ANIM_SCENE: '.. animScene)
+	    	Citizen.InvokeNative(0xF4D94AF761768700, animScene) 
+	    	if time then
+	    		Citizen.Wait(tonumber(time))	
+	    	else
+	   		Citizen.Wait(10000) 
+	    	end
 			
-	    -- SET CHEST AS OPENED AFTER DUG UP
-	    -- Citizen.InvokeNative(0x188F8071F244B9B8, chest, 1) -- found native sets CHEST as OPENED
-			
-	    -- _DELETE_ANIM_SCENE
-	    Citizen.InvokeNative(0x84EEDB2C6E650000, animScene) 
-   end) 
+	    	-- SET CHEST AS OPENED AFTER DUG UP
+	    	-- Citizen.InvokeNative(0x188F8071F244B9B8, chest, 1) -- found native sets CHEST as OPENED		
+	    	
+		-- _DELETE_ANIM_SCENE
+	    	Citizen.InvokeNative(0x84EEDB2C6E650000, animScene) 
+   	end) 
 end
 
 --- DIG UP AND FIND NOTHING
